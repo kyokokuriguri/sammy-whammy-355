9760cd3eaafaf324296e7be6c2e499ef37af4f6c
RAY <82920511+kyokokuriguri@users.noreply.github.com>
2024-09-10 15:13:37 -0700

    fixed all my code
    fixed from erlier and added sprint

--- sammy whammy 355/Assets/scripts/playercontroler.cs
+++ sammy whammy 355/Assets/scripts/playercontroler.cs
@@ -2,31 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playercontroler : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    Rigidbody axle;
-    Camera PlayerCam;
+    Rigidbody myRB;
+    Camera playerCam;
+
     Vector2 camRotation;
-    public float speed = 10f;
-    public float jump = 5f;
+
+    public bool sprintMode = false;
+
+    [Header("Movement Settings")]
+    public float speed = 10.0f;
+    public float sprintMultiplier = 2.5f;
+    public float jumpHeight = 5.0f;
+    public float groundDetectDistance = 1f;
+
+    [Header("User Settings")]
+    public bool sprintToggleOption = false;
     public float mouseSensitivity = 2.0f;
-    [Header("user settings")]
-    public bool sprintToggleOption = false; 
     public float Xsensitivity = 2.0f;
     public float Ysensitivity = 2.0f;
-    public float camRotationLimit = 2.0f;
-    public float sprintMultiplier = 2.5f;
-        public bool sprintMode = false;
-    //public float groundDetectionDistance = 1;
+    public float camRotationLimit = 90f;
+
     // Start is called before the first frame update
     void Start()
     {
-        axle = GetComponent<Rigidbody>();
-        PlayerCam = transform.GetChild(0).GetComponent<Camera>();
+        myRB = GetComponent<Rigidbody>();
+        playerCam = transform.GetChild(0).GetComponent<Camera>();
+
         camRotation = Vector2.zero;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
-
     }
 
     // Update is called once per frame
@@ -34,35 +40,47 @@ public class playercontroler : MonoBehaviour
     {
         camRotation.x += Input.GetAxisRaw("Mouse X") * mouseSensitivity;
         camRotation.y += Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+
         camRotation.y = Mathf.Clamp(camRotation.y, -camRotationLimit, camRotationLimit);
-        PlayerCam.transform.localRotation = Quaternion.AngleAxis(camRotation.y, Vector3.left);
+
+        playerCam.transform.localRotation = Quaternion.AngleAxis(camRotation.y, Vector3.left);
         transform.localRotation = Quaternion.AngleAxis(camRotation.x, Vector3.up);
 
-        Vector3 temp = axle.velocity;
-        temp.x = Input.GetAxisRaw("Vertical") * speed;
-        temp.z = Input.GetAxisRaw("Horizontal") * speed;
-        if (Input.GetKeyDown(KeyCode.Space) && Physics. Raycast(transform.position, -transform.up,1))//can use groundDetectDistance too
-            temp.y = jump;
-        axle.velocity = (temp.x * transform.forward) + (temp.z * transform.right) + (temp.y * transform.up);
-        if(sprintToggleOption)
-            {
+        Vector3 temp = myRB.velocity;
+
+        float verticalMove = Input.GetAxisRaw("Vertical");
+        float horizontalMove = Input.GetAxisRaw("Horizontal");
+
+        if(!sprintToggleOption)
+        {
             if (Input.GetKey(KeyCode.LeftShift))
                 sprintMode = true;
-            if (Input.GetKey(KeyCode.LeftShift))
+
+            if (Input.GetKeyUp(KeyCode.LeftShift))
                 sprintMode = false;
         }
-        if (Input.GetKeyDown(KeyCode.LeftShift) && temp.x > 0)
-        {
-            sprintMode = false;
-        }
-        if (Input.GetKey(KeyCode.LeftShift))
-            sprintMode = true;
-        if(sprintMode)
-                temp.x = Input.GetAxisRaw("Vertical") * speed;
-        if (sprintMode)
-                temp.x = Input.GetAxisRaw("Vertical") * speed;
-        if (Input.GetKey(KeyCode.LeftShift))
-            sprintMode = false;
 
+        if(sprintToggleOption)
+        {
+            if (Input.GetKey(KeyCode.LeftShift) && verticalMove > 0)
+                sprintMode = true;
+
+            if (verticalMove <= 0)
+                sprintMode = false;
+        }
+
+        if(!sprintMode)
+            temp.x = verticalMove * speed;
+
+        if(sprintMode)
+            temp.x = verticalMove * speed * sprintMultiplier;
+
+        temp.z = horizontalMove * speed;
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && Physics.Raycast(transform.position, -transform.up, groundDetectDistance))
+            temp.y = jumpHeight;
+
+        myRB.velocity = (temp.x * transform.forward) + (temp.z * transform.right) + (temp.y * transform.up);
     }
 }
